87e1e1a0c0d6ee3ddbbedfafd2292123603e1d56
Unknown <theweeryan17@gmail.com>
2017-12-18 14:00:01 -0600

    More merch fixes

--- Code/js/merch.js
+++ Code/js/merch.js
@@ -1,27 +1,28 @@
-$(document).ready(function(){
-    if($(window).width() > 480) {
+$(document).ready(function () {
+    if ($(window).width() > 480) {
         $(".extra").slideUp(1);
-        
-        $(".item").hover(function(){
+
+        $(".item").hover(function () {
             $(this).find(".extra").slideDown(1000);
-        },function(){
+        }, function () {
             $(this).find(".extra").slideUp(1000);
         });
-        var discription = false;
-        $(".header").click(function(){
-            if(discription) {
-                $(this).parent().find(".info").slideUp(1000);
-                $(this).find("#arrow").text("\u25BC");
-                discription = false;
-            } else {
-                $(this).parent().find(".info").slideDown(1000);
-                $(this).find("#arrow").text("\u25B2");
-                discription = true;
-            }
-        });
-        $(".order").click(function(){
-            window.location = "orderForm.html?item=" + $(this).parent().parent().parent().parent().find(".main").find("p").html()
-        });
     }
-    
+    $(window).resize(function() {
+        
+    });
+    var discription = false;
+    $(".btn-info").click(function () {
+        if (discription) {
+            $(this).parent().parent().parent().find(".info").slideUp(1000);
+            discription = false;
+        } else {
+            $(this).parent().parent().parent().find(".info").slideDown(1000);
+            discription = true;
+        }
+    });
+    $(".order").click(function () {
+        window.location = "orderForm.html?item=" + $(this).parent().parent().parent().parent().find(".main").find("p").html()
+    });
+
 });
